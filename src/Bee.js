f765f7f2aeb787bbb31b52093694ce4b6d7b4398
Drewclam <andrew.c.lam94@gmail.com>
2017-04-03 12:07:14 -0700

    All classes pass specs

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+  // this.food is inherited from superclass Grub
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+// eat method is inherited from superclass Grub
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  // color property inherited from superclass Bee
+  // food property falls through superclass Bee to it's superclass Grub
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+// eat method falls through superclass Bee to it's superclass Grub
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
